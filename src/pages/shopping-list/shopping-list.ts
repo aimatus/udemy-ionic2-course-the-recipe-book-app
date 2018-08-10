edc629543dcc89a008affb4c15dfc8a7fe2a908e
Abraham Matus <aimatusco@gmail.com>
2018-08-10 10:56:27 -0500

    Adds form to Shopping List elements.

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ShoppingListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage } from 'ionic-angular';
+import { NgForm } from '../../../node_modules/@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ShoppingListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onAddItem(form: NgForm) {
+    console.log(form);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ShoppingListPage');
-  }
-
 }
