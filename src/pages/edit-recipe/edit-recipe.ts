c586a911ed6981054cfaab7624b021bb48e7457a
Abraham Matus <aimatusco@gmail.com>
2018-08-12 18:10:34 -0500

    Adds form controls to edit-recipe page.

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -10,10 +11,24 @@ export class EditRecipePage implements OnInit {
 
   mode = 'New';
   difficultyOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
   constructor(public navParams: NavParams) { }
 
   ngOnInit(): void {
     this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl(this.difficultyOptions[1], Validators.required)
+    });
   }
 }
