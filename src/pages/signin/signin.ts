1cf886d257f72358c50cf36d3b9edb32a309866c
Abraham Matus <aimatusco@gmail.com>
2018-08-19 16:12:54 -0500

    Refactors signin class code.

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
@@ -10,15 +10,42 @@ import { AuthService } from '../../services/auth.service';
 })
 export class SigninPage {
 
-  constructor(private authService: AuthService) { }
+  loadingDialog: Loading;
+
+  constructor(
+    private authService: AuthService,
+    private loadingController: LoadingController,
+    private alertController: AlertController) { }
 
   onSignin(form: NgForm) {
-    this.authService.signin(form.value.email, form.value.password)
+    this.createAndInitLoaderSpinner();
+    this.signinAndHideLoadingSpinner(form.value.email, form.value.password);
+  }
+
+  createAndInitLoaderSpinner() {
+    this.loadingDialog = this.loadingController.create({
+      content: 'Signing you up...'
+    });
+    this.loadingDialog.present();
+  }
+
+  signinAndHideLoadingSpinner(email: string, password: string) {
+    this.authService.signin(email, password)
       .then(data => {
-        console.log(data);
+        this.loadingDialog.dismiss();
       })
       .catch(error => {
-        console.log(error);
+        this.loadingDialog.dismiss();
+        this.showSignupErrorAlert(error.message);
       });
+  }
+
+  showSignupErrorAlert(message: string) {
+    const alert = this.alertController.create({
+      title: 'Signin failed!',
+      message: message,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 }
