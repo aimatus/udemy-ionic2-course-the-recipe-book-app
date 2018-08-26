978083e951c960cf94198b0523d417043fd2cd9b
Abraham Matus <aimatusco@gmail.com>
2018-08-25 21:27:50 -0500

    Adds services for storing and fetching recipes.

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, PopoverController } from 'ionic-angular';
+import { IonicPage, NavController, PopoverController, LoadingController, Loading, AlertController, ToastController } from 'ionic-angular';
 import { EditRecipePage } from '../edit-recipe/edit-recipe';
 import { Recipe } from '../../models/recipe.model';
 import { RecipesService } from '../../services/recipes.service';
 import { RecipePage } from '../recipe/recipe';
 import { RecipesOptionsPage } from './recipes-options/recipes-options';
+import { AuthService } from '../../services/auth.service';
+import { RecipesConstants } from './recipes-constants';
 
 @IonicPage()
 @Component({
@@ -13,12 +15,17 @@ import { RecipesOptionsPage } from './recipes-options/recipes-options';
 })
 export class RecipesPage {
 
+  loadingSpinner: Loading;
   recipes: Recipe[];
 
   constructor(
     private navController: NavController,
     private recipesService: RecipesService,
-    private popoverController: PopoverController) { }
+    private popoverController: PopoverController,
+    private loadingController: LoadingController,
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private authService: AuthService) { }
 
   ionViewWillEnter() {
     this.recipes = this.recipesService.getRecipes();
@@ -34,8 +41,72 @@ export class RecipesPage {
   }
 
   onShowOptions(event) {
-    console.log('asdasd');
     const popover = this.popoverController.create(RecipesOptionsPage);
     popover.present({ ev: event });
+    popover.onDidDismiss(
+      action => {
+        this.execDismissAction(action);
+      }
+    );
+  }
+
+  private execDismissAction(action: any) {
+    this.loadingSpinner = this.loadingController.create({
+      content: 'Please wait...'
+    });
+    this.authService.getActiveUser().getIdToken()
+      .then((token: string) => {
+        if (action === RecipesConstants.LOAD_POPOVER_ACTION) {
+          this.loadingSpinner.present();
+          this.fetchIngredientsList(token);
+        }
+        else if (action === RecipesConstants.SAVE_POPOVER_ACTION) {
+          this.loadingSpinner.present();
+          this.storeIngredientsList(token);
+        }
+      });
+  }
+
+  private storeIngredientsList(token: string) {
+    this.recipesService.storeRecipesList(token)
+      .subscribe(() => {
+        this.loadingSpinner.dismiss();
+        this.notifiyEvent('Recipes saved');
+      }, error => {
+        this.loadingSpinner.dismiss();
+        this.handleError(error.error.error);
+      });
+  }
+
+  private fetchIngredientsList(token: string) {
+    this.recipesService.fetchRecipesList(token)
+      .subscribe((recipes: Recipe[]) => {
+        if (recipes) {
+          this.recipes = recipes;
+        }
+        this.loadingSpinner.dismiss();
+        this.notifiyEvent('Recipes loaded');
+      }, error => {
+        console.log(error);
+        this.loadingSpinner.dismiss();
+        this.handleError(error.error.error);
+      });
+  }
+
+  private handleError(errorMessage: string) {
+    const alert = this.alertController.create({
+      title: 'An error ocurred!',
+      message: errorMessage,
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
+  private notifiyEvent(message: string) {
+    console.log(message);
+    this.toastController.create({
+      message: message,
+      duration: 1500
+    }).present();
   }
 }
--- src/services/recipes.service.ts
+++ src/services/recipes.service.ts
@@ -1,12 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Recipe } from "../models/recipe.model";
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import 'rxjs/Rx'
 
+@Injectable()
 export class RecipesService {
 
     private recipes: Recipe[] = [];
 
+    constructor(
+        private httpClient: HttpClient,
+        private authService: AuthService) { }
+
     addRecipe(recipe: Recipe) {
         this.recipes.push(recipe);
-        console.log(this.recipes);
     }
 
     getRecipes() {
@@ -19,5 +27,33 @@ export class RecipesService {
 
     removeRecipe(index: number) {
         this.recipes.splice(index, 1);
+    }
+
+    storeRecipesList(token: string) {
+        return this.httpClient
+            .put(this.buildIngredientsUri(token), this.recipes)
+            .map((response: Recipe[]) => {
+                return response
+            });
+    }
+
+    fetchRecipesList(token: string) {
+        return this.httpClient
+            .get(this.buildIngredientsUri(token))
+            .map((response: Recipe[]) => {
+                return response;
+            })
+            .do((data) => {
+                this.recipes = data;
+            });
+    }
+
+    private buildIngredientsUri(token: string) {
+        const userId = this.authService.getActiveUser().uid;
+        const domain = 'https://ionic2-the-recipe-book.firebaseio.com/';
+        const authToken = '?auth=' + token;
+        const recipesListJsonFile = '/recipes-list.json';
+        const ingredientsUri = domain + userId + recipesListJsonFile + authToken;
+        return ingredientsUri;
     }
 }
