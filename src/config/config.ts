18b012307fea168d2238cc44a0dffc80142ebf46
Abraham Matus <aimatusco@gmail.com>
2018-08-26 14:38:04 -0500

    Adds firebase configuration file.

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+export var config = {
+  // Add your Firebase configuration here:
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
+  }
+}
